0b03e106b2f07135d64c0bc084c9e3f5b157df19
Filip Zymek <fzy@spyro-soft.com>
2023-07-28 13:48:14 +0200

    signin/up flow impl sample

--- AmplifySlowSignin/ContentView.swift
+++ AmplifySlowSignin/ContentView.swift
@@ -1,17 +1,45 @@
 //
-
+//
+import Amplify
+import AWSCognitoAuthPlugin
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var username: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, \(username)")
+            }
+
+            Button("Sign out") {
+                Task {
+                    await Amplify.Auth.signOut()
+                }
+            }
         }
         .padding()
+        .task {
+            do {
+                try await greetUser()
+            } catch {
+                print("error fetching user details: \(error)")
+            }
+        }
     }
+
+    @MainActor
+    func greetUser() async throws {
+        let user = try await Amplify.Auth.getCurrentUser()
+        username = user.username
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
